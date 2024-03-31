53a751f4e9793f3133cb6da8608842c9cc0d413b
Lisset <lissetchopper@gmail.com>
2024-03-30 20:25:00 -0600

    Feat:: Bind Style

---
+++ src/components/BindStyle.vue
@@ -0,0 +1,11 @@
+<template>
+    <p v-bind:style="{backgroundColor : '#ccc', color : '#fff'}">El mensaje se ha enviado satisfactoriamente.</p>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- src/components/ClassBind.vue
+++ src/components/ClassBind.vue
@@ -1,8 +1,10 @@
 <template>
     <p :class="{'danger' : hasError}">El mensaje se a enviado satisfactoriamente.</p>
+
 </template>
 
 <script lang="ts">
+    //v-bind (: y ya con eso tambien) asocia datos template-estructura por bindeo de calse en OptionAPI
       import { defineComponent } from 'vue';
       export default defineComponent({
         name: 'ClassBind',
