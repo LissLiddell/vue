3333f15759dfb0c171e26418b804e83f461fd5c4
Lisset <lissetchopper@gmail.com>
2024-03-30 18:51:41 -0600

    Feat:: v-if, v-else, v-else-if VS v-show

--- src/components/ExampleSelective.vue
+++ src/components/ExampleSelective.vue
@@ -1,6 +1,15 @@
 <template>
+
+    <!--v-if:Pintar o no pintar nodo en virtual dom VS v-show:Pintando todos los nodos en virtual dom pero con css lo oculta-->
     <h1 v-if="age < 18"> Eres menor de edad</h1>
     <h1 v-else> Eres mayor de edad</h1>
+
+    <h2 v-show="age < 18"> Eres menor de edad</h2>
+    <h2 v-show="age > 18"> Eres mayor de edad</h2>
+
+    <!-- v-if cuando el pintado se pinte pocos veces-->
+    <!-- v-show cuando el render sea muy frecuente-->
+
 </template>
 
 <script lang="ts" setup>
