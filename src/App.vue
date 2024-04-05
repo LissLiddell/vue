9470c6211b01609b581ffe8a45b6a23d3b046ef9
Liss <lissetchopper@gmail.com>
2024-04-05 14:37:06 -0600

    Feat:: Practica v-bind Array Interface

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,12 @@
 <template>
-  <FormOption  />
+  <TeacherForm />
 </template>
 
 <script lang="ts" setup>
-  //import ExampleSelective from './components/ExampleSelective.vue'
-  //import  ExampleFor from './components/ExampleFor.vue'
-
-import FormOption from './components/FormOption.vue'
+  import TeacherForm from './components/TeacherForm.vue'
 </script>
 
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/TeacherForm.vue
@@ -0,0 +1,61 @@
+<template>
+    <section>
+        <h3>Añadir profesor</h3>
+        <div><label>Nombre:</label> <input type="text" v-model="teacher.teacherName" /></div>
+        <div><label>Apellido Materno:</label> <input type="text" v-model="teacher.lastName1" /></div>
+        <div><label>Apellido Paterno:</label> <input type="text" v-model="teacher.lastName2" /></div>
+        <div><label>DNI / NIF:</label> <input type="text" v-model="teacher.dni" /></div>
+        <div><label>Materias:</label> <input type="text" v-model="subject" /> <button @click="handleSubject">Agregar</button></div>
+        <div>
+            <ul>
+                <li v-for="(elm,index) in teacher.subjects" :key="index"> {{ elm }}</li>
+            </ul>
+        </div>
+        <input type="checkbox" v-model="teacher.doc" /> Documentacion Entregada
+        <button>Agregar</button>
+    </section>
+
+    <section>
+        <h3>Listado de profesores</h3>
+
+    </section>
+</template>
+
+<script lang="ts" setup>
+    import { Ref, ref } from 'vue'
+//Modelo de gestion de info que se escribe en el formulario
+    //ts
+    interface ITeacher {
+    teacherName: string,
+    lastName1: string,
+    lastName2: string,
+    dni: string,
+    subjects: Array<string>,
+    doc: boolean   
+    }
+
+    //js
+   let teacher:Ref<ITeacher> = ref ({
+        teacherName: '',
+        lastName1: '',
+        lastName2: '',
+        dni: '',
+        subjects: [], //arreglo
+        doc: false
+    })
+
+// Arreglo de materias para profesores
+    let teachers:Ref<Array<ITeacher>> = ref([])
+
+    let subject:Ref<string> = ref('')
+
+// insertar materia a arreglo materias
+    const handleSubject = () => {
+        teacher.value.subjects.push(subject.value)
+    }
+        
+</script>
+
+<style scoped>
+
+</style>
