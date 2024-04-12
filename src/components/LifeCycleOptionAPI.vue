ce1563a0b88eaa8748fe34f0d208bdf7289d0202
Liss <lissetchopper@gmail.com>
2024-04-11 18:55:36 -0600

    Feat:: Ciclo de vida componente en optionAPI, compositionAPI

---
+++ src/components/LifeCycleOptionAPI.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>Hola mundo</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: "LifeCycleOptionAPI",
+ //   data() {
+      
+ //   },
+ //   methods:{
+
+//    },
+    created(){
+      console.log("created");
+    },
+    mounted(){
+      console.log("mounted");
+    },
+    updated(){
+      console.log("updated");
+    },
+    unmounted(){
+      console.log("unmounted");
+    }
+  })
+</script>
+
+<style >
+
+</style>
